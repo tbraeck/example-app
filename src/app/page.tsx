b5c2e41b623ce125c38bbb0865e17a74d3b6d368
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-07-06 10:50:59 -0600

    container, get images going

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
 import Box from '@mui/material/Box';
-
+import { Container } from "@mui/material";
 
 export default function Home() {
   return (
     <main>
-      <Box>
-        <h1>Several Sausages Sat on Simpson</h1>
-      </Box>
-      <Box>
-        <Image src="https://i.pinimg.com/originals/5b/f3/b3/5bf3b3ff8b27eefc4abcee898af6b9a0.gif" width={100}  height={100} alt="" title="kitten" />
-      </Box>
+      <Container>
+        <Box>
+          <h1>Several Sausages Sat on Simpson</h1>
+        </Box>
+        <Box>
+          <Image src="https://i.pinimg.com/originals/5b/f3/b3/5bf3b3ff8b27eefc4abcee898af6b9a0.gif" width={500}  height={500} alt="" title="kitten" />
+        </Box>
+      </Container> 
     </main>
   );
 }
