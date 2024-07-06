6549296af16607e6e58bce076a6e7229c7a8ec0f
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-07-06 10:34:25 -0600

    config for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'assets.example.com',
-          port: '',
-          pathname: '/account123/**',
-        },
-      ],
+      domains: ['i.pinimg.com', 'example.com'], // Add other domains as needed
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+      formats: ['image/avif', 'image/webp'],
     },
+    webpack(config, options) {
+      return config;
+    }
   };
-  
-  module.exports = nextConfig;
   
   export default nextConfig;
   
